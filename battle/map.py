514a6ebeadac9dd3f4334554771511c941b115b6
Mathis Hiron <mathis05.hiron@outlook.fr>
2025-11-03 15:04:10 +0100

    création fichier boucle de jeu avec test de la map

--- battle/map.py
+++ battle/map.py
@@ -41,28 +41,3 @@ class SparseMap:
                 s += "." if (x, y) not in self.grille else str(self.grille[(x, y)])[0]
             s += "\n"
         return s
-
-
-maptest = SparseMap(5, 5)
-print()
-print("map initialisée :")
-print(maptest)
-
-maptest.ajouter(1, 1, "K")
-maptest.ajouter(3, 2, "A")
-maptest.ajouter(4, 4, "P")
-
-print("map avec 3 unités :")
-print(maptest)
-
-maptest.deplacer(1, 1, 2, 1)
-print("Après déplacement du chevalier :")
-print(maptest)
-
-maptest.retirer(3, 2)
-print("Après retirer archer :")
-print(maptest)
-
-print("Case (2,1) :", maptest.obtenir(2, 1))
-print("Case (0,0) vide ?", maptest.est_vide(0, 0))
-print()
